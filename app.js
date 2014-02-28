f6bc3f91b277b748d28aa59b457f5d44aaca53fa
Freddy Montano <fmontano@symplicity.com>
2014-02-28 13:53:58 -0500

    Adding socket.io events.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var util = require('util'),
 	colors = require('colors'),
 	app = require('express.io')(),
 	spawn = require('child_process').spawn,
+	io = require('socket.io-client'),
 	state = {
 		'success': ['success', 'D\/DroidGap', 'D\/CordovaLog'],
 		'error': ['error', 'E\/'],
@@ -15,6 +16,10 @@ app.get('/', function(req, res) {res.sendfile(__dirname + '/public/client.html')
 app.get('/js/jquery-1.9.1.min.js', function(req, res) {res.sendfile(__dirname + '/public/js/jquery-1.9.1.min.js');});
 app.get('/js/bootstrap.min.js', function(req, res) {res.sendfile(__dirname + '/public/js/bootstrap.min.js');});
 app.get('/css/bootstrap.min.css', function(req, res) {res.sendfile(__dirname + '/public/css/bootstrap.min.css');});
+
+/// Trying to connect to logcat-server to send logs
+var socket = io.connect('http://127.0.0.1:8888');
+socket.on('connect', function () { }); /// Left here for later use
 
 var parseStdout = function(data, _class) {
 	data.toString().split('\n').forEach(function(line) {
@@ -38,15 +43,19 @@ var parseStdout = function(data, _class) {
 			if(type.indexOf('error') >= 0) {
 				console.log(line.red.bold);
 				app.io.broadcast('line', {'line': line, 'type': 'error'});
+				socket.emit('log', {'line': line, 'type': 'error' }, function(){});
 			} else if(type.indexOf('warning') >= 0) {
 				console.log(line.yellow.bold);
 				app.io.broadcast('line', {'line': line, 'type': 'warning'});
+				socket.emit('log', {'line': line, 'type': 'warning' }, function(){});
 			} else if(type.indexOf('success') >= 0) {
 				console.log(line.green.bold);
 				app.io.broadcast('line', {'line': line, 'type': 'success'});
+				socket.emit('log', {'line': line, 'type': 'success' }, function(){});
 			} else {
 				console.log(line.blue.bold);
 				app.io.broadcast('line', {'line': line, 'type': type[0]});
+				socket.emit('log', {'line': line, 'type': type[0] }, function(){});
 			}
 		}
 	});
